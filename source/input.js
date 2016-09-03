840b84cb496cc75b8e0d35bfd4bf51aa7a72eeae
Nikolay <halt.hammerzeit.at@gmail.com>
2016-09-03 11:02:48 +0300

    onBlur fix

--- source/input.js
+++ source/input.js
@@ -138,11 +138,11 @@ export default class Phone_input extends React.Component
 	// This handler is mainly for `redux-form`
 	on_blur(event)
 	{
-		const { onBlur, value, format } = this.props
+		const { onBlur, format } = this.props
 
 		if (onBlur)
 		{
-			onBlur(plaintext_international(value, format))
+			onBlur(plaintext_international(this.input_element().value, format))
 		}
 	}
 
