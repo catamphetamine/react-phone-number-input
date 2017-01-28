5ecae9b98a4d31eb0c626bf7abfb52a17674ec8d
Nikolay <halt.hammerzeit.at@gmail.com>
2017-01-29 00:00:02 +0300

    Fix babel-register ignore on tests

---
+++ test/setup.js
@@ -0,0 +1,24 @@
+require('babel-register')
+({
+	// Optional ignore regex - if any filenames
+	// **do** match this regex then they aren't compiled.
+	// ignore: /regex/,
+
+	// Optional only regex - if any filenames
+	// **don't** match this regex then they aren't compiled.
+	// only: /my_es6_folder/,
+
+	// Ignore can also be specified as a function.
+	ignore: function(filename)
+	{
+		if (filename.indexOf('/node_modules/') >= 0)
+		{
+			if (filename.indexOf('/libphonenumber-js/') >= 0)
+			{
+				return false
+			}
+			return true
+		}
+		return false
+	},
+})
