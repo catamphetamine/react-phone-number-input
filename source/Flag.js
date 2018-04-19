7ccc5cdc5f1d617ae613f38f915e6cc1bb19b1e0
catamphetamine <purecatamphetamine@gmail.com>
2018-04-19 13:18:05 +0300

    Removed semicolons

--- source/Flag.js
+++ source/Flag.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 // Default country flag icon.
 // `<img/>` is wrapped in a `<div/>` to prevent SVGs from exploding in size.
