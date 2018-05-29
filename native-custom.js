2a4796a9f39b9740238e4a8a1c4f76ec11ed0cc3
catamphetamine <purecatamphetamine@gmail.com>
2018-05-29 11:29:10 +0300

    Added `/native-custom` export for custom metadata

---
+++ native-custom.js
@@ -0,0 +1,4 @@
+var PhoneInput = require('./commonjs/PhoneInputNative').default
+
+exports = module.exports = PhoneInput
+exports['default']       = PhoneInput
---
+++ react-responsive-ui-custom.js
@@ -0,0 +1,4 @@
+var PhoneInput = require('./commonjs/PhoneInputReactResponsiveUI').default
+
+exports = module.exports = PhoneInput
+exports['default']       = PhoneInput
