4bba44d1e9f32e309335b224d170f730ebdde5bc
catamphetamine <purecatamphetamine@gmail.com>
2018-10-03 13:21:20 +0300

    react-styleguidist

--- react-styleguidist.js
+++ react-styleguidist.js
@@ -2,5 +2,10 @@ module.exports = {
 	"components": "source/PhoneInput.js",
 	"styleguideDir": "bundle/docs/styleguide",
 	"usageMode": "expand",
-	sortProps: props => props
+	sortProps: props => props,
+	dangerouslyUpdateWebpackConfig(webpackConfig, env) {
+		webpackConfig.output.filename = 'build/bundle.js'
+		webpackConfig.output.chunkFilename = 'build/[name].js'
+		return webpackConfig
+	}
 }
