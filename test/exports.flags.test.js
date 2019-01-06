34ee19983fa729d666b56aaf480fe8693243d283
catamphetamine <purecatamphetamine@gmail.com>
2019-01-06 09:17:01 +0300

    travis test fix

--- test/exports.flags.test.js
+++ test/exports.flags.test.js
@@ -3,10 +3,10 @@ import Flags from '../flags/index'
 describe('exports/flags', () => {
 	it('should export ES6', () => {
 		Flags.RU.should.be.a('function')
-	}).timeout(5000)
+	}).timeout(60000)
 
 	it('should export CommonJS', () => {
 		const Library = require('../flags/index.commonjs')
 		Library.default.RU.should.be.a('function')
-	}).timeout(5000)
+	}).timeout(60000)
 })
