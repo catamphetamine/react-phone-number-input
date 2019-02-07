67ed2167a53aa725f48c753032c8eb07e72f44eb
catamphetamine <purecatamphetamine@gmail.com>
2019-02-08 02:20:21 +0300

    TypeScript issue link

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,6 @@ export function formatPhoneNumber(value?: string): string;
 export function formatPhoneNumber(value: string, format?: NumberFormat): string;
 export function formatPhoneNumberIntl(value?: string): string;
 export function isValidPhoneNumber(value?: string): boolean;
+
+// See:
+// https://github.com/catamphetamine/react-phone-number-input/issues/195
