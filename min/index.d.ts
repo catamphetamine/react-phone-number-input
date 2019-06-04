460e44d8902fd120d9e0334646ef622ca369adc7
hazratgs <hazratgs@gmail.com>
2019-06-04 13:47:19 +0300

    Typescript type support in min and max

---
+++ min/index.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="react">
+declare module "react-phone-number-input/min" {
+
+  export type NumberFormat = "NATIONAL" | "National" | "INTERNATIONAL" | "International";
+
+  export function formatPhoneNumber(value?: string): string;
+  export function formatPhoneNumber(value: string, format?: NumberFormat): string;
+
+  export function formatPhoneNumberIntl(value?: string): string;
+
+  export function isValidPhoneNumber(value?: string): boolean;
+
+  export type FlagsMap = { [countryCode: string]: React.Component<object, object> };
+
+  export default isValidPhoneNumber
+}
+
+// See:
+// https://github.com/catamphetamine/react-phone-number-input/issues/195
