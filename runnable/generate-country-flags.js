e098de672009bdcb4e9f10cec9b6f19dc46c3e6f
catamphetamine <purecatamphetamine@gmail.com>
2019-06-20 23:38:55 +0300

    #258. Removed unused code.

--- runnable/generate-country-flags.js
+++ runnable/generate-country-flags.js
@@ -14,7 +14,7 @@ function generateFlags() {
 import React from "react"
 
 export default {${countries.map((country) => {
-	return '\n\t' + country + ': () => (\n' + filterSvgMarkup(getCountryFlagSvgMarkup(country)) + '\t)'
+	return '\n\t' + country + ': () => (\n' + getCountryFlagSvgMarkup(country) + '\t)'
 })}
 }
 	`.trim()
@@ -35,51 +35,6 @@ function getCountryFlagSvgMarkup(country) {
 	return code.replace('import React from "react";\n\nconst SvgComponent = props => (\n', '')
 		.replace(' {...props}', '')
 		.replace('\n);\n\nexport default SvgComponent;', '')
-}
-
-/**
- * Replaces HTML-style attributes with camelCased React ones.
- * The script used for checking:
- * var src = `...`
- * var attributes = [];
- * for (const attribute of src.match(/[a-zA-Z]+-[a-zA-Z]+="/g)) {
- *   if (!attributes.includes(attribute)){
- *     attributes.push(attribute)
- *   }
- * }
- * console.log(attributes.map(attribute => attribute.slice(0, attribute.length - '="'.length)).sort().join('\n'))
- * @param  {string} svgMarkup
- * @return {string}
- */
-function filterSvgMarkup(svgMarkup) {
-	const SVG_ATTRIBUTES = [
-		'clip-path',
-		'fill-opacity',
-		'fill-rule',
-		'font-family',
-		'font-size',
-		'font-weight',
-		'letter-spacing',
-		'stop-color',
-		'stop-opacity',
-		'stroke-dashoffset',
-		'stroke-miterlimit',
-		'stroke-opacity',
-		'stroke-width',
-		'stroke-linejoin',
-		'stroke-linecap',
-		'text-anchor',
-		'word-spacing',
-		'xmlns:xlink',
-		'xlink:href'
-	]
-	for (const attribute of SVG_ATTRIBUTES) {
-		svgMarkup = svgMarkup.replace(new RegExp(` (${attribute})="`, 'g'), (_, match) => ` ${match.replace(/[-:]([a-z])/g, (_, letter) => letter.toUpperCase())}="`)
-	}
-	return svgMarkup
-		// Won't work for things like "<g clip-path="url(#a)">".
-		// // Remove `id`s.
-		// .replace(/ id=".+?"/g, '')
 }
 
 // Leave only those countries supported by `libphonenumber-js`.
