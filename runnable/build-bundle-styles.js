28c6864beb7d1256b4c36a8cfa009cce66d8cf31
catamphetamine <purecatamphetamine@gmail.com>
2020-01-01 14:04:30 +0300

    CSS variables fix

--- runnable/build-bundle-styles.js
+++ runnable/build-bundle-styles.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 var autoprefixer = require('autoprefixer')
+var postcssCustomProperties = require('postcss-custom-properties')
 var postcss = require('postcss')
 
 RegExp.escape = function (string) {
@@ -17,7 +18,10 @@ String.prototype.replace_all = function (what, with_what) {
 function transformStyle(inPath, outPath) {
   outPath = outPath || inPath
   const text = fs.readFileSync(path.join(__dirname, inPath), 'utf8')
-  return postcss([autoprefixer()]).process(text, { from: undefined }).then((result) => {
+  return postcss([
+    autoprefixer(),
+    postcssCustomProperties()
+  ]).process(text, { from: undefined }).then((result) => {
     result.warnings().forEach((warn) => console.warn(warn.toString()))
     fs.writeFileSync(path.join(__dirname, outPath), result.css)
   })
